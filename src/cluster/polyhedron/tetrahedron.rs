450034629e93178cecc6cdb611187271289603a4
Lukas Trombach <TrombachL@gmail.com>
2022-03-06 19:18:48 +1300

    changes to tetrahedron

--- src/cluster/polyhedron/tetrahedron.rs
+++ src/cluster/polyhedron/tetrahedron.rs
@@ -12,18 +12,19 @@ pub fn new(n_layers: &u16) -> Cluster {
 }
 
 fn generate_coords(n_layers: &u16) -> Coordinates {
-    let trans_matrix = Matrix3d::new(
-        0.0,
-        1.0 / 2_f64.sqrt(),
-        1.0 / 2_f64.sqrt(),
-        1.0 / 2_f64.sqrt(),
-        0.0,
-        1.0 / 2_f64.sqrt(),
-        1.0 / 2_f64.sqrt(),
-        1.0 / 2_f64.sqrt(),
-        0.0,
-    );
-    let non_cart_coord = non_cart_coord(n_layers);
+    trans_matrix() * non_cart_coord(n_layers)
+}
 
-    trans_matrix * non_cart_coord
+fn trans_matrix() -> Matrix3d {
+    Matrix3d::new(
+        0.0,
+        1.0 / 2_f64.sqrt(),
+        1.0 / 2_f64.sqrt(),
+        1.0 / 2_f64.sqrt(),
+        0.0,
+        1.0 / 2_f64.sqrt(),
+        1.0 / 2_f64.sqrt(),
+        1.0 / 2_f64.sqrt(),
+        0.0,
+    )
 }

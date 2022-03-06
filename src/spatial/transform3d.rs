a78bd132defd6abda3e9eafcf5f3e0f6430ef9d5
Lukas Trombach <TrombachL@gmail.com>
2022-03-06 14:32:24 +1300

    use const generics for rotation matrices

--- src/spatial/transform3d.rs
+++ src/spatial/transform3d.rs
@@ -2,51 +2,67 @@ use crate::spatial::coord3d;
 use crate::spatial::matrix3d;
 use std::f64::consts;
 
-fn angle(deg: f64, rep: f64) -> f64 {
+const fn angle(deg: f64, rep: f64) -> f64 {
     2.0 * consts::PI * deg / rep
 }
 
-/// Computes a general rotation matrix given values for
-///
-/// `deg`: the degree of the rotation (e.g. 5 for a C5 rotation)
-///
-/// `rep`: how many times the rotation should be applied
-///
-/// `axis`: the rotational axis
-pub fn rot_mat(deg: u32, rep: u32, axis: &coord3d::Coord3d) -> matrix3d::Matrix3d {
-    if deg > rep {
+/**
+Computes a general rotation matrix given values for
+
+`DEG`: the degree of the rotation (e.g. 5 for a C5 rotation)
+
+`REP`: how many times the rotation should be applied
+
+`axis`: the rotational axis
+
+# Example
+```
+let rot_mat = rot_mat::<5, 1>(&rot_axis_normed);
+```
+*/
+pub fn rot_mat<const DEG: u8, const REP: u8>(axis: &coord3d::Coord3d) -> matrix3d::Matrix3d {
+    if DEG > REP {
         panic!("deg is required to be greater than rep")
     };
 
-    let deg = f64::from(deg);
-    let rep = f64::from(rep);
+    const DEG: f64 = f64::from(DEG);
+    const REP: f64 = f64::from(REP);
 
-    let angle = angle(deg, rep);
+    const ANGLE: f64 = angle(DEG, REP);
 
     let matrix = [
-        angle.cos() + f64::powi(axis.x, 2) * (1.0 - angle.cos()),
-        axis.x * axis.y * (1.0 - angle.cos()) - axis.z * angle.sin(),
-        axis.x * axis.z * (1.0 - angle.cos()) + axis.y * angle.sin(),
-        axis.y * axis.x * (1.0 - angle.cos()) + axis.z * angle.sin(),
-        angle.cos() + f64::powi(axis.y, 2) * (1.0 - angle.cos()),
-        axis.y * axis.z * (1.0 - angle.cos()) - axis.x * angle.sin(),
-        axis.z * axis.x * (1.0 - angle.cos()) - axis.y * angle.sin(),
-        axis.z * axis.y * (1.0 - angle.cos()) - axis.x * angle.sin(),
-        angle.cos() * f64::powi(axis.z, 2) * (1.0 - angle.cos()),
+        ANGLE.cos() + f64::powi(axis.x, 2) * (1.0 - ANGLE.cos()),
+        axis.x * axis.y * (1.0 - ANGLE.cos()) - axis.z * ANGLE.sin(),
+        axis.x * axis.z * (1.0 - ANGLE.cos()) + axis.y * ANGLE.sin(),
+        axis.y * axis.x * (1.0 - ANGLE.cos()) + axis.z * ANGLE.sin(),
+        ANGLE.cos() + f64::powi(axis.y, 2) * (1.0 - ANGLE.cos()),
+        axis.y * axis.z * (1.0 - ANGLE.cos()) - axis.x * ANGLE.sin(),
+        axis.z * axis.x * (1.0 - ANGLE.cos()) - axis.y * ANGLE.sin(),
+        axis.z * axis.y * (1.0 - ANGLE.cos()) - axis.x * ANGLE.sin(),
+        ANGLE.cos() * f64::powi(axis.z, 2) * (1.0 - ANGLE.cos()),
     ];
 
     matrix3d::Matrix3d::from(matrix)
 }
 
-/// Computes an improper rotation matrix given values for
-///
-/// `deg`: the degree of the rotation (e.g. 5 for a C5 rotation)
-///
-/// `rep`: how many times the rotation should be applied
-///
-/// `axis`: the rotational axis
-pub fn improper_rot_mat(deg: u32, rep: u32, axis: &coord3d::Coord3d) -> matrix3d::Matrix3d {
-    if rep % 2 == 1 {
+/**
+Computes an improper rotation matrix given values for
+
+`DEG`: the degree of the rotation (e.g. 5 for a C5 rotation)
+
+`REP`: how many times the rotation should be applied
+
+`axis`: the rotational axis
+
+# Example
+```
+let rot_mat = improper_rot_mat::<5, 1>(&rot_axis_normed);
+```
+*/
+pub fn improper_rot_mat<const DEG: u8, const REP: u8>(
+    axis: &coord3d::Coord3d,
+) -> matrix3d::Matrix3d {
+    if REP % 2 == 1 {
         panic!("only valid for odd rep")
     };
 
@@ -60,5 +76,9 @@ pub fn improper_rot_mat(deg: u32, rep: u32, axis: &coord3d::Coord3d) -> matrix3d
         -axis.z * axis.x * 2.0,
         -axis.z * axis.y * 2.0,
         1.0 - f64::powi(axis.z, 2) * 2.0,
-    ]) * rot_mat(deg, rep, axis)
+    ]) * rot_mat::<DEG, REP>(axis)
+}
+
+pub fn inversion() -> matrix3d::Matrix3d {
+    -matrix3d::Matrix3d::new_unit_matrix()
 }

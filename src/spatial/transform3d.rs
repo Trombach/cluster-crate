4268b6a669e413be8d01fe76f4550874875d5498
Lukas Trombach <TrombachL@gmail.com>
2022-03-07 18:05:35 +1300

    hide use statements from doc tests

--- src/spatial/transform3d.rs
+++ src/spatial/transform3d.rs
@@ -12,8 +12,8 @@ use std::f64::consts;
 ///
 /// # Example
 /// ```
-/// use cluster_crate::spatial::coord3d;
-/// use cluster_crate::spatial::transform3d;
+/// # use cluster_crate::spatial::coord3d;
+/// # use cluster_crate::spatial::transform3d;
 ///
 /// let rot_axis_normed = coord3d::Coord3d::from([1.0, 1.0, 1.0]);
 /// let rot_mat = transform3d::rot_mat::<5, 1>(&rot_axis_normed);
@@ -48,8 +48,8 @@ pub fn rot_mat<const DEG: u8, const REP: u8>(axis: &coord3d::Coord3d) -> matrix3
 ///
 /// # Example
 /// ```
-/// use cluster_crate::spatial::coord3d;
-/// use cluster_crate::spatial::transform3d;
+/// # use cluster_crate::spatial::coord3d;
+/// # use cluster_crate::spatial::transform3d;
 ///
 /// let rot_axis_normed = coord3d::Coord3d::from([1.0, 1.0, 1.0]);
 /// let rot_mat = transform3d::improper_rot_mat::<5, 1>(&rot_axis_normed);
@@ -76,7 +76,7 @@ pub fn improper_rot_mat<const DEG: u8, const REP: u8>(
 ///
 /// # Example
 /// ```
-/// use cluster_crate::spatial::transform3d;
+/// # use cluster_crate::spatial::transform3d;
 ///
 /// let inversion = transform3d::inversion();
 /// ```

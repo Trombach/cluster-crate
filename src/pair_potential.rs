b033a16a0ba8b5adb659f5264db48d0216cdfe17
Lukas Trombach <TrombachL@gmail.com>
2022-06-05 18:34:06 +1200

    first version of PairPotential trait

--- src/pair_potential.rs
+++ src/pair_potential.rs
@@ -1,9 +1,9 @@
-use crate::spatial::{Coord3d, Coordinates};
+use crate::{cluster::Coordinates, spatial::Coord3d};
 
 trait PairPotential {
     fn e(distance: f64) -> f64;
-    fn energy(&self, coords: &Coordinates) -> f64 {
-        let e: f64 = 0.0;
+    fn energy(coords: &Coordinates) -> f64 {
+        let mut e: f64 = 0.0;
         for x1 in coords {
             for x2 in coords {
                 e += Self::e(x1.dist(&x2));
@@ -13,17 +13,50 @@ trait PairPotential {
         e
     }
     fn de_dr(distance: f64) -> f64;
-    fn gradient(&self, coords: &Coordinates) -> Vec<Coord3d> {
-        let gradient = vec![Coord3d::from([0.0, 0.0, 0.0]); coords.size()];
+    fn gradient(coords: &Coordinates) -> Vec<Coord3d> {
+        let mut gradient = vec![Coord3d::from([0.0, 0.0, 0.0]); coords.len()];
         for (i, x1) in coords.iter().enumerate() {
             for (j, x2) in coords.iter().enumerate() {
-                let d = x1 - x2;
+                let d = *x1 - *x2;
                 let g = Self::de_dr(d.norm());
                 let gradient_value = d / d.norm() * g;
-                gradient[]
+                gradient[i] += gradient_value;
+                gradient[j] -= gradient_value;
             }
         }
 
         gradient
+    }
+    fn d2e_dr2(distance: f64) -> f64;
+    fn hessian(coords: &Coordinates) -> Vec<Vec<f64>> {
+        let mut hessian = vec![vec![0f64; 3 * coords.len()]; 3 * coords.len()];
+        for (i, x1) in coords.iter().enumerate() {
+            for (j, x2) in coords.iter().enumerate() {
+                let d = *x1 - *x2;
+                let r = d.norm();
+
+                // first and second derivative values
+                let de_dr = Self::de_dr(r);
+                let d2e_dr2 = Self::d2e_dr2(r);
+
+                // derivatives of r
+                let dvecr_dr = d.d_norm();
+                let d2rvecr_dr2 = d.dd_norm();
+
+                // calculate hessian elements by looping over all 6 coordinates of 1 pair of coordinates
+                for k in 0..2 {
+                    for l in 0..2 {
+                        let hessian_value = de_dr * d2rvecr_dr2.get_value(l, k)
+                            + d2e_dr2 * dvecr_dr[k] * dvecr_dr[l];
+
+                        hessian[3 * i + k][3 * i + l] += hessian_value;
+                        hessian[3 * i + k][3 * j + l] -= hessian_value;
+                        hessian[3 * j + k][3 * i + l] -= hessian_value;
+                        hessian[3 * j + k][3 * j + l] += hessian_value;
+                    }
+                }
+            }
+        }
+        hessian
     }
 }

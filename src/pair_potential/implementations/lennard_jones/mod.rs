7fd085ee8e7739fc08dbdaa2b085c4b3f0ee82a0
Lukas Trombach <TrombachL@gmail.com>
2022-06-06 16:10:03 +1200

    tests and changes for lennard jones

--- src/pair_potential/implementations/lennard_jones/mod.rs
+++ src/pair_potential/implementations/lennard_jones/mod.rs
@@ -1,42 +1,48 @@
 use crate::pair_potential::PairPotential;
 
+/// This implements a Lennard-Jones potential with variable exponents
+///
+/// `eps`: Depth of potential energy well
+///
+/// `re`: Equilibrium distance
+///
+/// `n`: Exponent of the repulsive term
+///
+/// `m`: Exponent of the attractive term
 pub struct LennardJonesPotential {
-    epsilon: f64,
-    rm: f64,
-    exp1: f64,
-    exp2: f64,
+    eps: f64,
+    re: f64,
+    n: f64,
+    m: f64,
 }
 
 impl LennardJonesPotential {
-    pub fn new(epsilon: f64, rm: f64, exp1: f64, exp2: f64) -> Self {
-        Self {
-            epsilon,
-            rm,
-            exp1,
-            exp2,
-        }
+    pub fn new(eps: f64, re: f64, n: f64, m: f64) -> Self {
+        assert!(
+            n > m,
+            "Repulsive exponent must be larger than attractive exponent."
+        );
+        Self { eps, re, n, m }
     }
 }
 
 impl PairPotential for LennardJonesPotential {
     fn e(&self, distance: f64) -> f64 {
-        self.epsilon / (self.exp1 / self.exp2 - 1.0)
-            * ((f64::powf(self.rm / distance, self.exp1))
-                - (self.exp1 / self.exp2) * (f64::powf(self.rm / distance, self.exp2)))
+        self.eps / (self.n / self.m - 1.0)
+            * ((f64::powf(self.re / distance, self.n))
+                - (self.n / self.m) * (f64::powf(self.re / distance, self.m)))
     }
 
     fn de_dr(&self, distance: f64) -> f64 {
-        -self.epsilon / (self.rm * (self.exp1 / self.exp2 - 1.0))
-            * (self.exp1 * (f64::powf(self.rm / distance, self.exp1 + 1.0))
-                - self.exp1 * (f64::powf(self.rm / distance, self.exp2 + 1.0)))
+        -self.eps / (self.re * (self.n / self.m - 1.0))
+            * (self.n * (f64::powf(self.re / distance, self.n + 1.0))
+                - self.n * (f64::powf(self.re / distance, self.m + 1.0)))
     }
 
     fn d2e_dr2(&self, distance: f64) -> f64 {
-        self.epsilon / (f64::powi(self.rm, 2) * (self.exp1 / self.exp2 - 1.0))
-            * ((f64::powi(self.exp1, 2) + self.exp1)
-                * f64::powf(self.rm / distance, self.exp1 + 2.0)
-                - (self.exp1 * self.exp2 + self.exp1)
-                    * f64::powf(self.rm / distance, self.exp2 + 2.0))
+        self.eps / (f64::powi(self.re, 2) * (self.n / self.m - 1.0))
+            * ((f64::powi(self.n, 2) + self.n) * f64::powf(self.re / distance, self.n + 2.0)
+                - (self.n * self.m + self.n) * f64::powf(self.re / distance, self.m + 2.0))
     }
 }
 
@@ -53,24 +59,22 @@ mod tests {
     fn create_pot() {
         let pot = get_pot();
 
-        println!("{}", (12.0 / 6.0 - 1.0));
-
-        assert_eq!(pot.epsilon, 1.0);
-        assert_eq!(pot.rm, 1.0);
-        assert_eq!(pot.exp1, 12.0);
-        assert_eq!(pot.exp2, 6.0);
+        assert_eq!(pot.eps, 1.0);
+        assert_eq!(pot.re, 1.0);
+        assert_eq!(pot.n, 12.0);
+        assert_eq!(pot.m, 6.0);
     }
 
     #[test]
     fn calc_e_value() {
         let pot = get_pot();
-        let e0 = pot.e(pot.rm);
-        let e_long_r = pot.e(pot.rm * 2.0);
-        let e_short_r = pot.e(pot.rm / 2.0);
+        let e0 = pot.e(pot.re);
+        let e_long_r = pot.e(pot.re * 2.0);
+        let e_short_r = pot.e(pot.re / 2.0);
         let e_lim_r_0 = pot.e(0.0);
         let e_lim_r_inf = pot.e(f64::INFINITY);
 
-        assert_eq!(e0, -1.0);
+        assert_eq!(e0, -pot.eps);
         assert!(e0 < e_long_r);
         assert!(e0 < e_short_r);
         assert!(f64::is_nan(e_lim_r_0));
@@ -80,9 +84,9 @@ mod tests {
     #[test]
     fn calc_g_value() {
         let pot = get_pot();
-        let g0 = pot.de_dr(pot.rm);
-        let g_far = pot.de_dr(pot.rm * 2.0);
-        let g_near = pot.de_dr(pot.rm / 2.0);
+        let g0 = pot.de_dr(pot.re);
+        let g_far = pot.de_dr(pot.re * 2.0);
+        let g_near = pot.de_dr(pot.re / 2.0);
         let g_lim_r_0 = pot.de_dr(0.0);
         let g_lim_r_inf = pot.de_dr(f64::INFINITY);
 
@@ -96,8 +100,16 @@ mod tests {
     #[test]
     fn calc_h_value() {
         let pot = get_pot();
-        let h0 = pot.d2e_dr2(pot.rm);
+        let h0 = pot.d2e_dr2(pot.re);
+        let h_far = pot.d2e_dr2(pot.re * 2.0);
+        let h_near = pot.d2e_dr2(pot.re / 2.0);
+        let h_lim_r_0 = pot.d2e_dr2(0.0);
+        let h_lim_r_inf = pot.d2e_dr2(f64::INFINITY);
 
-        println!("{}", h0);
+        assert_eq!(h0, 72.0);
+        assert!(f64::is_sign_negative(h_far));
+        assert!(f64::is_sign_positive(h_near));
+        assert!(f64::is_nan(h_lim_r_0));
+        assert_eq!(h_lim_r_inf, 0.0);
     }
 }
